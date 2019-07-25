203cf8c9f5a27ed1c6a6abe5038569f1738bb57c
permenent <2691556679@qq.com>
2019-07-25 17:37:14 +0800

    Commit

--- Heap/Heap/Heap.c
+++ Heap/Heap/Heap.c
@@ -9,20 +9,32 @@ void Swap(HPDataType*num1, HPDataType* num2)  //交换函数
 	*num2 = tmp;
 
 }
-void HeapInit(Heap* hp, HPDataType *a, int n) // 堆的初始化 建堆
+void HeapInit(Heap* hp, HPDataType *a, int n)
 {
-	  
-		hp->_a = malloc(sizeof(HPDataType)*n);
-		memcpy(hp->_a, a, sizeof(HPDataType)*n);
-		hp->_size = n;
-		hp->_capacity = n;
-	//构建堆
-	for (int i = (n - 1 - 1) / 2; i >= 0; i--) //调用向下调整法 构建堆。
+	hp->_a = malloc(sizeof(HPDataType)*n);
+	memcpy(hp->_a, a, sizeof(HPDataType)*n);
+	hp->_size = n;
+	hp->_capacity = n;
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
 	{
 		AdjustDown(hp->_a, hp->_size, i);
 	}
 }
-void AdjustDown(HPDataType* a, size_t n, size_t parent)
+//void HeapInit(Heap* hp, HPDataType *a, int n) // 堆的初始化 建堆
+//{
+//	  
+//		hp->_a = malloc(sizeof(HPDataType)*n);
+//		memcpy(hp->_a, a, sizeof(HPDataType)*n);
+//		hp->_size = n;
+//		hp->_capacity = n;
+//	//构建堆
+//	for (int i = (n - 1 - 1) / 2; i >= 0; i--) //调用向下调整法 构建堆。
+//	{
+//			AdjustDown(hp->_a, hp->_size, i);
+//	}
+//}
+
+void AdjustDown(HPDataType* a, size_t n, size_t parent) //建小堆
 {
 	int child = parent * 2 + 1;
 	while (child < n)
@@ -53,6 +65,19 @@ void HeapDestory(Heap* hp)	//销毁堆
 	}
 	hp->_size = hp->_capacity = 0;
 }
+//void HeapPush(Heap*hp, HPDataType x)   注意malloc会使失败 因为memcpy将 数组的空间和值赋给hp->_a
+//{
+//	if (hp->_size == hp->_capacity)
+//	{
+//		size_t newcapacity = hp->_capacity == 0 ? 4 : hp->_capacity * 2;
+//		hp->_a = (HPDataType*)malloc(sizeof(HPDataType)*newcapacity);
+//		hp->_capacity = newcapacity;
+//	}
+//	hp->_a[hp->_size] = x;
+//	hp->_size++;
+//	Ajustup(hp->_a, hp->_size - 1);
+//}
+
 void HeapPush(Heap* hp, HPDataType x)  // 堆的插入
 {
 	if (hp->_size == hp->_capacity)
@@ -68,7 +93,26 @@ void HeapPush(Heap* hp, HPDataType x)  // 堆的插入
 
 	Ajustup(hp->_a, hp->_size - 1 );
 }
-void Ajustup(int* a, size_t children) //堆向上调整法  children即为数组元素个数
+//void Ajustup(int *a, size_t child) //堆的向上调整法
+//{
+//	int parent = (child - 1) / 2;
+//	while (child =  0)
+//	{
+//		if (a[child] < a[parent])
+//		{
+//			Swap(&a[child], &a[parent]);
+//			child = parent;
+//			parent = (child - 1) / 2;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//}
+
+
+void Ajustup(int* a, size_t children) //堆向上调整法  children即为数组元素个数 O(logn)*n
 {
 	size_t parent = (children - 1) / 2;
 	//while (parent >=0)
@@ -86,8 +130,11 @@ void Ajustup(int* a, size_t children) //堆向上调整法  children即为数组元素个数
 		}
 	}
 }
-void HeapPop(Heap* hp) //清除 堆顶那个最小的
+
+
+void HeapPop(Heap* hp) //清除 堆顶那个最小的 O(logn)
 {
+	assert(hp->_size > 0);
 	Swap(&hp->_a[0], &hp->_a[hp->_size - 1]);
 	hp->_size--;
 	AdjustDown(hp->_a, hp->_size, 0);	
@@ -106,24 +153,46 @@ int HeapEmpty(Heap* hp)  // 判断堆是否为空
 {
 	return hp->_size == 0;
 }
-
-void HeapSort(int *a, int n) //升序 建大堆
+void Adjust(HPDataType* a, size_t n, size_t parent) //建大堆
 {
-	for (int i = (n - 2) / 2; i >= 0; --i)
+	int child = parent * 2 + 1;
+	while (child < n)
 	{
-		AdjustDown(a, n, i);
+		if (child + 1 < n && a[child] < a[child + 1])
+		{
+			child++;
+		}
+		if (a[child] > a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+}
+void HeapSort(int *a, int n)    //升序 建大堆
+{
+	for (int i = (n - 2) / 2; i >= 0; --i) 
+	{
+		Adjust(a, n, i);
 	}
 	
 	int end = n - 1;
 	while (end > 0)
 	{
 		Swap(&a[end], &a[0]);
-		AdjustDown(a, end, 0);
-		end--;
+		Adjust(a, end, 0);
+		--end;
 	}
 
 	
 }
+
 void Print(Heap* hp)
 {
 	for (int i = 0; i < hp->_size; i++)
@@ -132,19 +201,49 @@ void Print(Heap* hp)
 	}
 	printf("\n");
 }
-
 void TestHeap()
 {
 	Heap myheap;
-	//HPDataType* arr[] = {7,3,8,9,2,1,6 };
-	HPDataType arr[] = { 9, 2, 3, 5, 8, 4 };
-	HeapInit(&myheap, arr, sizeof(arr)/sizeof(int));
+	HPDataType *arr[] = { 5, 7, 3, 4, 1, 2, 6 };
+	for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+	printf("小堆 : \n");
+	HeapInit(&myheap, arr, sizeof(arr) / sizeof(int));
 	Print(&myheap);
-	 HeapPush(&myheap, -1);
+	/*printf("堆排序 : \n");
+	HeapSort((&myheap)->_a, sizeof(arr) / sizeof(int));
+	Print(&myheap);*/
+	printf("堆插入 9\n");
+	HeapPush(&myheap,9);
 	Print(&myheap);
-	HeapSort(arr, sizeof(arr)/sizeof(int));
+	printf("堆插入 -1\n");
+	HeapPush(&myheap, -1);
 	Print(&myheap);
-	//Print(&myheap);
+	printf("返回堆顶元素\n");
+	printf("%d",HeapTop(&myheap));
+	printf("\n");
+	printf("删除堆顶元素\n");
+	HeapPop(&myheap);
+	Print(&myheap);
+}
+//void TestHeap()
+//{
+//	Heap myheap;
+//	//HPDataType* arr[] = {7,3,8,9,2,1,6 };
+//	HPDataType arr[] = { 9, 2, 3, 5, 8, 4 };
+//	HeapInit(&myheap, arr, sizeof(arr)/sizeof(int));
+//	//Print(&myheap);
+//	Print(&myheap);
+//
+//	//HeapPush(&myheap, -1);
+//	//Print(&myheap);
+//	HeapSort(arr, sizeof(arr) / sizeof(HPDataType));
+//	Print(&myheap);
+//	//Print(&myheap);
 
 
 	/*for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
@@ -179,5 +278,17 @@ void TestHeap()
 	printf("\n");*/
 	//Print(myheap._a, sizeof(arr) / sizeof(int));
 	
-}
+//}
 
+//void TestHeap()
+//{
+//	int a[] = { 7, 5, 2, 0, 6, 8, 3, 9, 1 };
+//	Heap hp;
+//	HeapInit(&hp, a, sizeof(a) / sizezof(int));
+//	HeapPrint(&hp);
+//	HeapPush(&hp, 3);
+//	HeapPush(&hp, -1);
+//	HeapPrint(&hp);
+//	HeapPop(&hp);
+//	HeapPrint(&hp);
+//}
